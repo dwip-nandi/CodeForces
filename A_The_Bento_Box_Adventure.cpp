4c5a8d559ee296c01ef2e6d34da65b4155aaf0c1
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2024-12-07 23:29:25 +0600

    Simple Calculation
    2024 ICPC Asia Taichung Regional Contest (Unrated, Online Mirror, ICPC Rules, Preferably Teams)

---
+++ A_The_Bento_Box_Adventure.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    int sum =a+b+c+d;
+    int result = (1+2+3+4+5)-sum;
+    cout<<result<<endl;
+}
