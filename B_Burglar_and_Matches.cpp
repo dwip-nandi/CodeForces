23683419b0375b719e8e8524be54fac70d38d52e
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2024-12-24 00:00:17 +0600

    Update Code

--- B_Burglar_and_Matches.cpp
+++ B_Burglar_and_Matches.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n, m;
     cin >> n >> m;
@@ -11,19 +10,15 @@ int main() {
         p[i].first = a;
         p[i].second = b;
     }
-
     sort(p.rbegin(), p.rend(), [](const pair<int, int>& a, const pair<int, int>& b) {
         return a.second < b.second;
     });
-
     int sum = 0;
     for (int i = 0; i < m && n > 0; i++) {
         int take = min(n, p[i].first); 
         sum += take * p[i].second; 
         n -= take; 
     }
-
     cout << sum << endl;
-
     return 0;
 }
