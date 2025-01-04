f0892c62dbbbc339d48a7331990a400f3273822d
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-01-04 23:06:18 +0600

    Logical Algorithm

---
+++ A_MEX_Table.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, m;
+        cin >> n >> m;
+        cout<<max(n,m) +1<<endl;
+    }
+    return 0;
+}
