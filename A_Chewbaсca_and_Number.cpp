985494aac1e19f3436c98c424592d240ddd64931
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-01-07 14:04:29 +0600

    Number to string operation

---
+++ A_Chewbaсca_and_Number.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (i == 0 && s[i] == '9')
+        {
+            continue;
+        }
+        else if (s[i] > '4')
+        {
+            s[i] = '9' - s[i] + '0';
+        }
+    }
+    cout << s << endl;
+}
