ea9ee7320397275f5a3898d14f98f66dafe50f20
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-01-08 19:49:50 +0600

    Mathematical Implementation

---
+++ B_Coloring_Rectangles.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int x=ceil((double)(a*b)/3);
+        cout<<x<<endl;
+    }
+}
