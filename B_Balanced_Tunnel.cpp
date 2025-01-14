1e3b32ca81c7351d8e6443814d5bc6ba38ef5359
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-01-14 20:54:41 +0600

    High Logical Application

---
+++ B_Balanced_Tunnel.cpp
@@ -0,0 +1,67 @@
+/*#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> va(n);
+    vector<int> vb(n);
+    for (int i = 0; i < n; i++)
+        cin >> va[i];
+    for (int i = 0; i < n; i++)
+        cin >> vb[i];
+    int c=0;
+    for (int i = 0; i < va.size(); i++) {
+        for (int j = 0; j < vb.size(); j++) {
+            if (va[i] == vb[j]) {
+                if (j < i) {
+                    c++;
+                    i--;
+                    j--;
+                    vb.erase(vb.begin()+j);
+                    va.erase(va.begin()+i);
+                    break;
+                }
+            }
+        }
+    }
+    for(auto ub:va)cout<<ub<<' ';
+    cout<<endl;
+    for(auto ua:vb)cout<<ua<<' ';
+    cout<<endl;
+    cout << c << endl;
+
+    return 0;
+}*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> va(n);
+    vector<int> vb(n);
+    for (int i = 0; i < n; i++)
+        cin >> va[i];
+    for (int i = 0; i < n; i++)
+        cin >> vb[i];
+
+    int c = 0;
+    unordered_map<int, int> pos;
+    for (int i = 0; i < n; i++) {
+        pos[vb[i]] = i;
+    }
+
+    int max_pos = -1;
+    for (int i = 0; i < n; i++) {
+        if (pos[va[i]] < max_pos) {
+            c++;
+        } else {
+            max_pos = pos[va[i]];
+        }
+    }
+
+    cout << c << endl;
+
+    return 0;
+}
+
