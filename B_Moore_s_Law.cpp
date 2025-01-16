9762757e09a875118470bb02acf1217ef3ed6b8d
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-01-16 13:55:23 +0600

    B. Moore's Law(Mathematical Implementation)
    https://codeforces.com/problemset/problem/630/B

---
+++ B_Moore_s_Law.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    long long n,t;
+    cin>>n>>t;
+    double x = n*pow(1.000000011,t);
+    cout<<fixed<<setprecision(30)<<x<<endl;
+
+}
