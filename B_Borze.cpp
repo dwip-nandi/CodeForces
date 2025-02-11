5000fecbf04a27687eec0275215c1bfc43c42288
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-02-11 22:48:10 +0600

    Add files via upload

---
+++ B_Borze.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string borze;
+    cin >> borze;
+
+    string decoded;
+    for (int i = 0; i < borze.length(); ++i) {
+        if (borze[i] == '.') {
+            decoded += '0';
+        } else if (borze[i] == '-' && borze[i + 1] == '.') {
+            decoded += '1';
+            ++i; 
+        } else if (borze[i] == '-' && borze[i + 1] == '-') {
+            decoded += '2';
+            ++i; 
+        }
+    }
+
+    cout << decoded << endl;
+    return 0;
+}
