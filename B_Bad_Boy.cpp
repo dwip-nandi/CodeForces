e08c302c00101c8669c17a83d4c574a5c44ec6ba
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-03-06 22:35:32 +0600

    1537B - Bad Boy

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ll t; cin>>t;
+    while(t--){
+        ll x,y,i,j;
+        cin>>x>>y>>i>>j;
+        cout<<1<<' '<<1<<' '<<x<<' '<<y<<endl;
+    }
+}
