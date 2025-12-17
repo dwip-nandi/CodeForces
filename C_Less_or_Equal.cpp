de2dc2a0db8482f4d0437bd6bead2887f6d5966d
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-12-17 23:46:34 +0600

    977C - Less or Equal(update logic)

--- C_Less_or_Equal.cpp
+++ C_Less_or_Equal.cpp
@@ -1,50 +1,3 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// #define int long long
-// #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
-
-// void query() {
-//     int n,k;
-//     cin >>n>>k ;
-//     vector<int>v(n);
-//     map<int,int>mp;
-//     for(int i=0;i<n;i++){
-//         cin>>v[i];
-//         mp[v[i]]++;
-//     }
-//     if (k == 0) {
-//         if (v[0] == 1) cout << -1 <<endl;
-//         else cout << v[0] - 1 <<endl;
-//         return;
-//     }
-//     bool is=false;
-//     int z=0,va=-1;
-//     for(auto it:mp){
-//         z += it.second;
-//         if(z==k){
-//             va=it.first;
-//             is=true;
-//             break;
-//         }else if(z>k){
-//             is = false;
-//             break;
-//         }
-//     }
-//     if(is)cout<<va<<endl;
-//     else cout<<-1<<endl;
-// }
-
-// signed main() {
-//     fast_io;
-//     int t=1;
-//     //cin >> t;
-//     while (t--) {
-//         query();
-//     }
-//     return 0;
-// }
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -55,18 +8,36 @@ void query() {
     int n, k;
     cin >> n >> k;
     vector<int> v(n);
-    for (int i = 0; i < n; i++) cin >> v[i];
-    sort(v.begin(), v.end());
-
+    map<int,int> mp;
+    int mn = LLONG_MAX;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        mn = min(mn, v[i]);
+        mp[v[i]]++;
+    }
     if (k == 0) {
-        if (v[0] == 1) cout << -1 <<endl;
-        else cout << v[0] - 1 <<endl;
+        if (mn == 1) cout << -1 << endl;
+        else cout << mn - 1 << endl;
         return;
     }
 
-    int x = v[k-1];
-    if (k < n && v[k] == x) cout << -1 <<endl;
-    else cout << x <<endl;
+    int z = 0;
+    int ans = -1;
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        z += it->second;
+        if (z >= k) {
+            ans = it->first;
+            auto j = it; j++;
+            if (z > k && it->second > 1) {
+                ans = -1;
+            } else if (j != mp.end() && z == k && j->first == ans) {
+                ans = -1;
+            }
+            break;
+        }
+    }
+
+    cout << ans << endl;
 }
 
 signed main() {
@@ -74,3 +45,34 @@ signed main() {
     query();
     return 0;
 }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define int long long
+// #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+// void query() {
+//     int n, k;
+//     cin >> n >> k;
+//     vector<int> v(n);
+//     for (int i = 0; i < n; i++) cin >> v[i];
+//     sort(v.begin(), v.end());
+
+//     if (k == 0) {
+//         if (v[0] == 1) cout << -1 <<endl;
+//         else cout << v[0] - 1 <<endl;
+//         return;
+//     }
+
+//     int x = v[k-1];
+//     if (k < n && v[k] == x) cout << -1 <<endl;
+//     else cout << x <<endl;
+// }
+
+// signed main() {
+//     fast_io;
+//     query();
+//     return 0;
+// }
