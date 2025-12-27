94ed078d4e3ac4678cbf92edd81b17ca9a663d15
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-12-27 23:54:07 +0600

    A - Yes or Yes
    A - Yes or Yes(contest 27/12/25)

--- A_YES_or_YES.cpp
+++ A_YES_or_YES.cpp
@@ -1,27 +1,63 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define int long long
+// #define fast_io                  \
+//     ios::sync_with_stdio(false); \
+//     cin.tie(nullptr);
+
+// void query()
+// {
+//     string s;
+//     cin >> s;
+//     bool is = true;
+//     for (int i = 0; i <(s.size()-1); i++)
+//     {
+//         if (s[i] == 'Y' && s[i + 1] == 'Y')
+//         {
+//             is = false;
+//             break;
+//         }
+//     }
+//     if (is)
+//         cout << "YES" << endl;
+//     else
+//         cout << "NO" << endl;
+// }
+
+// signed main()
+// {
+//     fast_io;
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         query();
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-string to_upper(const string &s)
-{
-    string result = s;
-    transform(result.begin(), result.end(), result.begin(), ::toupper);
-    return result;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+void query() {
+    string s;
+    cin >> s;
+    int countY = count(s.begin(), s.end(), 'Y');
+    if (countY <= 1)
+        cout << "YES"<<endl;
+    else
+        cout << "NO"<<endl;
 }
-int main()
-{
+
+int main() {
+    fast_io;
     int t;
     cin >> t;
-    while (t--)
-    {
-        string s;
-        cin >> s;
-        string se = to_upper(s);
-        if (se == "YES")
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+    while (t--) {
+        query();
     }
+    return 0;
 }
