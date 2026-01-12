b0f74d64a4dafcd40d68fd53f33d56d82a9c0dba
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2026-01-13 01:05:29 +0600

    1399C - Boats Competition(Updated)

--- C_Boats_Competition.cpp
+++ C_Boats_Competition.cpp
@@ -1,28 +1,63 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int t;
+//     cin>>t;
+//     while(t--){
+//         int n;
+//         cin>>n;
+//         vector<int>v(n);
+//         map<int,int>mp;
+//         for(int i=0;i<n;i++){
+//             cin>>v[i];
+//             mp[v[i]]++;
+//         }
+//         int tc=0;
+//         for(int i=2;i<=2*n;i++){
+//             int cc=0;
+//             for(auto const&[w,c]:mp){
+//                   int ta=i-w;
+//                   if(ta>0 && mp.count(ta)){
+//                      cc +=min(c,mp.at(ta));
+//                   }
+//             }
+//             tc = max(tc,cc/2);
+//         }
+//         cout<<tc<<endl;
+//     }
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int n;
-        cin>>n;
-        vector<int>v(n);
-        map<int,int>mp;
-        for(int i=0;i<n;i++){
-            cin>>v[i];
-            mp[v[i]]++;
+        cin >> n;
+        vector<int> v(n);
+        map<int, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            m[v[i]]++;
         }
-        int tc=0;
-        for(int i=2;i<=2*n;i++){
-            int cc=0;
-            for(auto const&[w,c]:mp){
-                  int ta=i-w;
-                  if(ta>0 && mp.count(ta)){
-                     cc +=min(c,mp.at(ta));
-                  }
+        int mc = 0;
+        for (int s = 2; s <= 2 * n; s++)
+        {
+            int cc = 0;
+            for (auto const &[w, c] : m)
+            {
+                int d = s - w;
+                if (d > 0 && m.count(d))
+                {
+                    cc += min(c, m.at(d));
+                }
             }
-            tc = max(tc,cc/2);
+            mc = max(mc, cc/2);
         }
-        cout<<tc<<endl;
+        cout << mc << endl;
     }
 }
