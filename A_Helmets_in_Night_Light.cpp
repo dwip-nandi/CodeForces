10e2fc2cd8c131b417b01ad37bacc1a5cbe5f556
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2026-04-03 22:33:45 +0600

    1876A - Helmets in Night Light
    update

--- A_Helmets_in_Night_Light.cpp
+++ A_Helmets_in_Night_Light.cpp
@@ -1,3 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+void query()
+{
+    int n, p;
+    cin >> n >> p;
+
+    multimap<int, int> mp;
+    vector<int> va(n), vb(n);
+
+    for (int i = 0; i < n; ++i)
+        cin >> va[i];
+    for (int i = 0; i < n; ++i)
+        cin >> vb[i];
+
+    for (int i = 0; i < n; ++i)
+    {
+        mp.emplace(vb[i], va[i]);
+    }
+
+    int c = p, r = n - 1;
+
+    for (const auto &[b, a] : mp)
+    {
+        if (b > p || r <= 0)
+            break;
+
+        int x = min(r, a);
+        c += (b * x);
+        r -= x;
+    }
+
+    if (r > 0)
+    {
+        c += r * p;
+    }
+
+    cout << c << endl;
+}
+
+signed main()
+{
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        query();
+    }
+    return 0;
+}
+
 // #include <bits/stdc++.h>
 // using namespace std;
 
@@ -8,33 +66,35 @@
 //     int n, p;
 //     cin >> n >> p;
 
-//     multimap<int, int> mp;
 //     vector<int> va(n), vb(n);
+//     for (int i = 0; i < n; i++) cin >> va[i];
+//     for (int i = 0; i < n; i++) cin >> vb[i];
 
-//     for (int i = 0; i < n; ++i) cin >> va[i];
-//     for (int i = 0; i < n; ++i) cin >> vb[i];
-
-//     for (int i = 0; i < n; ++i) {
-//         mp.emplace(vb[i], va[i]);
+//     vector<pair<int, int>> pairs;
+//     for (int i = 0; i < n; i++) {
+//         pairs.push_back({vb[i], va[i]});
 //     }
+//     sort(pairs.begin(), pairs.end());
 
-//     int c = p, r = n - 1;
+//     int c = p;
+//     int r = n - 1;
 
-//     for (const auto& [b, a] : mp) {
-//         if (a <= p && r>=b) {
-//             c += (b*a);
-//             r -=b;
-//             if (r <= 0) break;
-//         } else {
-//             break;
-//         }
+//     for (auto& edge : pairs) {
+//         int cost = edge.first;
+//         int limit = edge.second;
+
+//         if (cost >= p || r <= 0) break;
+
+//         int d = min(r, limit);
+
+//         c += (d * cost);
+//         r -= d;
 //     }
-
 //     if (r > 0) {
-//         c += r * p;
+//         c += (r * p);
 //     }
 
-//     cout << c <<endl;
+//     cout << c << endl;
 // }
 
 // signed main() {
@@ -46,54 +106,3 @@
 //     }
 //     return 0;
 // }
-
-#include <bits/stdc++.h>
-using namespace std;
-
-#define int long long
-#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
-
-void query() {
-    int n, p;
-    cin >> n >> p;
-
-    vector<int> va(n), vb(n);
-    for (int i = 0; i < n; i++) cin >> va[i];
-    for (int i = 0; i < n; i++) cin >> vb[i];
-
-    vector<pair<int, int>> pairs;
-    for (int i = 0; i < n; i++) {
-        pairs.push_back({vb[i], va[i]});
-    }
-    sort(pairs.begin(), pairs.end());
-
-    int c = p; 
-    int r = n - 1;
-
-    for (auto& edge : pairs) {
-        int cost = edge.first;
-        int limit = edge.second;
-
-        if (cost >= p || r <= 0) break;
-
-        int d = min(r, limit);
-        
-        c += (d * cost);
-        r -= d;
-    }
-    if (r > 0) {
-        c += (r * p);
-    }
-
-    cout << c << endl;
-}
-
-signed main() {
-    fast_io;
-    int t;
-    cin >> t;
-    while (t--) {
-        query();
-    }
-    return 0;
-}
